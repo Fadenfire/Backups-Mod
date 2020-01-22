d7a209c7e7c8286ecef22921031c9f29363c17b6
Jon <xJon@users.noreply.github.com>
2020-01-23 01:11:34 +0200

    Update RestoreServerBackup.bat
    - Backups will be searched for in `backupsmod` first then in `backups`
    - Added more information for which backup was used

--- RestoreServerBackup.bat
+++ RestoreServerBackup.bat
@@ -18,26 +18,37 @@ goto :done
 :foundmod
 set modpath=mods\%modname%
 
-if [%backupdir%]==[] (
-	for /f "tokens=*" %%a in (
-		'dir /b /ad-h /t:c /o-d "backups"'
-	) do for /f "tokens=*" %%b in (
-			'dir /b /ad-h /t:c /o-d "backups\%%a"'
-		) do (
-			set latestworld=%%a
-			set latestsave=%%b
-			goto :foundbackup 
-		)
-	echo No backups were found
+if not [%backupdir%]==[] goto :backup
+
+if exist backupsmod (
+	set mainbackupsdir=backupsmod
+) else if exist backups (
+	set mainbackupsdir=backups
+) else (
+	echo No backups were found 
 	goto :done
-	:foundbackup
-	set backupdir=backups\%latestworld%\%latestsave%
 )
 
+for /f "tokens=*" %%a in (
+	'dir /b /ad-h /t:c /o-d "%mainbackupsdir%"'
+) do for /f "tokens=*" %%b in (
+		'dir /b /ad-h /t:c /o-d "%mainbackupsdir%\%%a"'
+	) do (
+		set latestworld=%%a
+		set latestsave=%%b
+		goto :foundbackup 
+	)
+echo No backups were found
+goto :done
+
+:foundbackup
+set backupdir=%mainbackupsdir%\%latestworld%\%latestsave%
+
+:backup
 java -jar "%modpath%" "%backupdir%" "%worlddir%"
 
 if %errorlevel%==0 (
-	echo World backup successfully restored to %worlddir%!
+	echo World backup successfully restored to [%worlddir%] from [%backupdir%]
 )
 
 :done
